0933955b8ee781a1e6853d7f0eec0b7c471d7fe3
James <jhagerty@alumni.cmu.edu>
2023-01-20 23:05:51 -0500

    now do trinary sentences

--- data/make_cards_dataset.py
+++ data/make_cards_dataset.py
@@ -1,5 +1,10 @@
+"""
+-Cards images and minimal vocabulary needed for cards images
+"""
+
+
 import os
-from preprocessing import standardize_image
+from preprocessing import standardize_image, trinary_image_sentence, vocabulary_json
 
 
 ################################################################################
@@ -13,10 +18,19 @@ DATASET_DIR = "./cards_dataset/"
 
 
 def main():
-    for filename in os.listdir(IMAGES_DIR):
-        input_path = f"{IMAGES_DIR}{filename}"
-        output_path = f"{DATASET_DIR}{filename}"
-        standardize_image(input_path, output_path)
+    # # Images
+    # for filename in os.listdir(IMAGES_DIR):
+    #     input_path = f"{IMAGES_DIR}{filename}"
+    #     output_path = f"{DATASET_DIR}{filename}"
+    #     standardize_image(input_path, output_path)
+    # Vocabulary
+    words = set()
+    for filename in os.listdir(DATASET_DIR):
+        cardname = filename.replace(".png", "")
+        words |= set(cardname.split("_"))
+        sentence = trinary_image_sentence(f"{DATASET_DIR}/{filename}")
+        break
+    print(words)
 
 if __name__ == "__main__":
     main()
--- data/preprocessing.py
+++ data/preprocessing.py
@@ -11,9 +11,14 @@ from PIL import Image as im
 
 
 IMG_DIM = 60
-# (grayscale) 0-85 -> black, 85-170 -> mid-gray, 170-255-> white
+IMG_WORD_DIM = 3
+
+# (Grayscale) 0-85 -> black, 85-170 -> mid-gray, 170-255-> white
 BLACK_MAX = 85
 WHITE_MIN = 170
+BLACK = 0
+GRAY = 128
+WHITE = 255
 
 
 ################################################################################
@@ -24,10 +29,11 @@ def crop():
 
 
 def standardize_image(input_path, output_path):
+    global BLACK_MAX, WHITE_MIN, BLACK, GRAY, WHITE
     def trinary_brightness(brightness):
-        if (brightness <= BLACK_MAX): return 0
-        elif (BLACK_MAX < brightness < WHITE_MIN): return 128
-        else: return 255
+        if (brightness <= BLACK_MAX): return BLACK
+        elif (BLACK_MAX < brightness < WHITE_MIN): return GRAY
+        else: return WHITE
     image = im.open(input_path)
     image = image.resize((IMG_DIM, IMG_DIM))
     image = image.convert("L") # grayscale
@@ -38,8 +44,32 @@ def standardize_image(input_path, output_path):
 ################################################################################
 
 
+def trinary_image_sentence(input_path):
+    global BLACK, GRAY, WHITE
+    print(input_path)
+    image = im.open(input_path)
+    rows, cols = image.size
+    grayscale_pixels = image.load()
+    grayscale_to_trinary = {BLACK : "0", GRAY : "1", WHITE : "2"}
+    trinary_pixels = [grayscale_to_trinary[grayscale_pixels[c, r]] 
+        for r in range(rows) for c in range(cols)]
+    print(len(trinary_pixels))
+    # TBD, sentence itself
+
+
+################################################################################
+
+
+def vocabulary_json(words):
+    token_to_id = {"[PAD]" : 0, "[MASK]" : 1, "[DESC]" : 2, "[IMG]" : 3}
+
+    id_to_token = {token_to_id[k] : k for k in token_to_id}
+    json_data = {"token_to_id" : token_to_id, "id_to_token" : id_to_token}
+
+
+################################################################################
 def main():
-    pass
+    standardize_image("./images/dog.png", "./images/test.png")
 
 if __name__ == "__main__":
     main()
